4c4315f36db66ef628bb92c082fae6ed5ff7dc76
Yoda <shixi.gs@gmail.com>
2022-09-12 18:43:03 +0800

    init

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const isForkModeEnabled = !!process.env.FORK_URL
+const forkUrl = process.env.FORK_URL
+const forkStartingBlock =
+  parseInt(process.env.FORK_STARTING_BLOCK, 10) || undefined
+const gasPrice = parseInt(process.env.GAS_PRICE, 10) || 0
+const chainId = parseInt(process.env.CHAIN_ID) || 1337
+
+const config = {
+  networks: {
+    hardhat: {
+      gasPrice,
+      initialBaseFeePerGas: 0,
+      chainId,
+      accounts: [
+        {
+          privateKey: "052054ca3aa7fb2cb9c8c84a3e537c7fed4cc1173a5ac76817599256f6d7d54a",
+          balance: "10000000000000000000000"
+        }
+      ]
+    },
+  },
+  analytics: { enabled: false },
+}
+
+if (isForkModeEnabled) {
+  console.log(`Running hardhat in a fork mode! URL: ${forkUrl}`)
+  if (forkStartingBlock) {
+    console.log(`Starting block: ${forkStartingBlock}`)
+  }
+  config.networks.hardhat.forking = {
+    url: forkUrl,
+    blockNumber: forkStartingBlock,
+  }
+} else {
+  console.log('Running with a fresh state...')
+}
+
+module.exports = config
